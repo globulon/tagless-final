f0ac796ccc115f2d2ae19c68df86fd78bfe19b14
Marc-Daniel Ortega <globulon@gmail.com>
2018-05-05 21:00:10 +0100

    [FIX] small overlap of definitions

--- src/main/scala/com/omd/tagless/Extensions.scala
+++ src/main/scala/com/omd/tagless/Extensions.scala
@@ -2,11 +2,9 @@ package com.omd.tagless
 
 
 final class Extensions[R](exps: ExpSYM[R], muls: MulSYM[R]) {
+  import exps._
+  import muls._
+
   def tfm1: R = add (lit (7)) (neg (mul(lit (1))(lit (2))))
   def tfm2: R = add (lit (7)) (new Expressions[R](exps).tf1)
-
-  private def mul : R ⇒ R ⇒ R = muls.mul
-  private def lit : Int  ⇒ R  = exps.lit
-  private def neg : R ⇒ R     = exps.neg
-  private def add : R ⇒ R ⇒ R = exps.add
 }
