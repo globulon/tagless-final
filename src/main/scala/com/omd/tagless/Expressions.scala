d6709d459dec1bad0785c9541c283d8cc6505913
Marc-Daniel Ortega <globulon@gmail.com>
2018-05-05 22:34:38 +0100

    [IMP] Reach wanted form as from lecture

--- src/main/scala/com/omd/tagless/Expressions.scala
+++ src/main/scala/com/omd/tagless/Expressions.scala
@@ -2,13 +2,31 @@ package com.omd.tagless
 
 
 trait Expression[R] {
-  def tf1 : R
+  def tf1(implicit ev: ExpSYM[R]) : R
+  def tfm1(implicit ev: ExpSYM[R], ev1: MulSYM[R]): R
+  def tfm2(implicit ev: ExpSYM[R], ev1: MulSYM[R]): R
 }
 
 trait Expressions {
-  final protected def expression[R](exps: ExpSYM[R]): Expression[R] = new Expression[R] {
-    import exps._
+  final protected def expression[R]: Expression[R] = new Expression[R] {
 
-    override def tf1: R = add (lit(8)) (neg(add(lit(1))(lit(2))))
+    //In tagless final lecture : ExpSYM repr ⇒ repr
+    override def tf1(implicit ev: ExpSYM[R]): R = {
+      import ev._
+      add (lit(8)) (neg(add(lit(1))(lit(2))))
+    }
+
+    //In tagless final lecture : (ExpSYM repr, MulSYM repr) ⇒ repr
+    override def tfm1(implicit ev: ExpSYM[R], ev1: MulSYM[R]): R = {
+      import ev._
+      import ev1._
+      add (lit (7)) (neg (mul(lit (1))(lit (2))))
+    }
+
+    //In tagless final lecture : (ExpSYM repr, MulSYM repr) ⇒ repr
+    override def tfm2(implicit ev: ExpSYM[R], ev1: MulSYM[R]): R = {
+      import ev._
+      add (lit (7)) (tf1)
+    }
   }
 }
--- src/main/scala/com/omd/tagless/Run.scala
+++ src/main/scala/com/omd/tagless/Run.scala
@@ -1,20 +1,13 @@
 package com.omd.tagless
 
 
-object Run extends Extensions with Expressions {
-  private def symbols[R : ExpSYM]: ExpSYM[R] = implicitly[ExpSYM[R]]
-  private def extended[R : MulSYM]: MulSYM[R] = implicitly[MulSYM[R]]
-
-  private def exp[R : ExpSYM]: Expression[R] = expression(symbols[R])
-  private def all[R : ExpSYM : MulSYM]: Extension[R] = extension[R](symbols[R], extended[R])
-
-
+object Run extends Expressions {
   def main(args: Array[String]): Unit = {
-    println(eval(all[Int].tfm1))
-    println(eval(all[String].tfm1))
-    println(eval(all[Int].tfm2))
-    println(eval(all[String].tfm2))
+    println(eval(expression[Int].tfm1))
+    println(eval(expression[String].tfm1))
+    println(eval(expression[Int].tfm2))
+    println(eval(expression[String].tfm2))
 
-    println(eval(exp[Tree].tf1))
+    println(eval(expression[Tree].tf1))
   }
 }
