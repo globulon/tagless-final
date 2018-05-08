69241ce384f2607f6f1fc52584a09216d1bfbbfe
Marc-Daniel Ortega <globulon@gmail.com>
2018-05-08 23:43:44 +0100

    [TEST] Defines GADT

--- src/test/scala/com/omd/tags/ExpressionsTest.scala
+++ src/test/scala/com/omd/tags/ExpressionsTest.scala
@@ -8,6 +8,14 @@ final class ExpressionsTest extends WordSpec with MustMatchers with Expressions 
       eval(List.empty)(ti1) must be (UB(true))
     }
 
+
+    /**
+    * Informally,
+    * our embedding is not ‘tight’: the algebraic data type Exp contains more val-
+    * ues than there are well-typed terms in the simply typed lambda-calculus with
+    * booleans. The embedding failed to represent the well-formedness constraints im-
+    * posed by the object language’s type system.
+    * * */
     "blow up" in {
       assertThrows[MatchError] {
         eval(List.empty)(ti2o)
